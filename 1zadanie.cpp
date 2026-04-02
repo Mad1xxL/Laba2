db8fc16c489d5f31f8a5b2609c691175af8a1cba
Максим <mad1x@MacBook-Pro-Maksim.local>
2026-04-02 19:13:09 +0700

    Created a code to calculate the maximum length in binary strings in C++

---
+++ 1zadanie.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+bool isBinary(const std::string& s) {
+    for (char c : s)
+        if (c != '0' && c != '1') return false;
+    return !s.empty();
+}
+
+int main() 
+{
+    std::string s;
+    std::cout << "Введите бинарную строчку: ";
+    std::cin >> s;
+
+    if (!isBinary(s)) {
+        std::cout << "Ошибка: строка должна содержать только 0 и 1! ";
+        return 1;
+    }
+
+    int k;
+    std::cout << "Введите количество 0, которые поменяются на 1: ";
+    std::cin >> k;
+
+    int left = 0, zeros = 0, maxLen = 0;
+
+    for (int right = 0; right < s.size(); right++) {
+        if (s[right] == '0') zeros++;
+        while (zeros > k) {
+            if (s[left] == '0') zeros--;
+            left++;
+        }
+
+        if (right - left + 1 > maxLen)
+            maxLen = right - left + 1;
+    }
+    
+    std::cout << "Максимальная длина: " << maxLen << std::endl;
+    return 0;
+}
